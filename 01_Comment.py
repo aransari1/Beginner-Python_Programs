82e3e99aede47b1df0319d4166b1c6ff99967ee8
Abdurrahman Ansari <abdurrahmanansari03@gmail.com>
2024-07-17 13:55:19 +0530

    Create 01_Comment.py
    Use the Cmment.py file to learn about the comments and its usage in Python.

---
+++ 01_Comment.py
@@ -0,0 +1,35 @@
+###################### Python Comments ######################
+
+# Information: Comments in Python is used to insert information related to the code or the program's working. For the other person to read and understand the code properly.
+
+
+# Note: Comments are not executed during the code execution, so please don't insert any code you want to execute in the comment's line.
+
+# Example: This code will not be executed during the Execution phase --> print ("This string is not executed during the execution")
+
+
+# To use the single line comment in Python, we need to use (# symbol) before the comment. anything written in the right side of the # will be treated as the comment.
+
+# Single line comment example:
+
+# This is the example of single line comment. You can not use comment in the next line without using the # symbol, else it will show you an error.
+
+''' To use multi-line comment, you need to surround the text with triple (single/double quotes)
+which will be applicable until you insert the triple (single/double quotes) in the end.'''
+
+# Multi-line comment example:
+
+"""
+This is 
+the example
+of double quotes
+multi-line
+comment."""
+
+'''
+This is
+the example
+of single quotes
+multi-line
+comment.
+'''
