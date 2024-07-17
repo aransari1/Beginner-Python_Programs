ffd449ef5c3d30016e3475aa4720f9781ea331ae
Abdurrahman Ansari <abdurrahmanansari03@gmail.com>
2024-07-17 15:36:29 +0530

    Created new files

---
+++ 2_Python_Syntax_&_Keyword.py
@@ -0,0 +1,77 @@
+###################### Python Syntax and Keywords ######################
+
+'''Python has a clean and readable syntax that emphasizes code readability. 
+Here are some key aspects of Python syntax and keywords:'''
+
+###################### Indentation ######################
+'''
+Python uses indentation to define code blocks, such as loops and functions. Indentation is significant in Python, unlike many other programming languages that use curly braces or keywords to define code blocks.'''
+
+x = 5
+if x > 0:
+    print("Positive")
+else:
+    print("Negative")
+
+###################### Keywords ######################
+'''Python has a set of reserved keywords that have special meanings and cannot be used as variable names or identifiers. Some common keywords include:
+
+- if, else, elif: Used for conditional statements
+- for, while: Used for loops
+- def: Used to define functions
+- class: Used to define classes
+- import: Used to import modules
+- print: Used to output values
+- return: Used to return values from functions
+'''
+
+###################### Variables and Assignments ######################
+'''
+Variables in Python are assigned values using the = operator. Python is dynamically typed, so variables can hold values of different data types without explicit declaration.
+'''
+
+x = 5
+name = "John"
+
+###################### Data Types ######################
+'''
+Python supports various built-in data types, including:
+
+- Integers (int): 42
+- Floating-point numbers (float): 3.14
+- Strings (str): "Hello, World!"
+- Booleans (bool): True or False
+- Lists (list): [1, 2, 3]
+- Tuples (tuple): (1, 2, 3)
+- Dictionaries (dict): {"name": "John", "age": 30}
+'''
+
+###################### Operators ######################
+'''
+Python provides a wide range of operators for performing various operations, such as arithmetic, comparison, and logical operations.
+'''
+#Example:
+
+x = 10
+y = 3
+print(x + y)  # Addition
+print(x - y)  # Subtraction
+print(x * y)  # Multiplication
+print(x / y)  # Division
+print(x > y)  # Comparison
+print(x and y)  # Logical AND
+
+###################### Functions ######################
+'''
+Functions in Python are defined using the `def` keyword followed by the function name and parameters (if any).
+'''
+#Example:
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Alice")  # Output: Hello, Alice!
+
+
+#These are just a few examples of Python syntax and keywords.
+#As we progress in our Python learning, we'll encounter more advanced concepts and features that build upon these fundamentals.
