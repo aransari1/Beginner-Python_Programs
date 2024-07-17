f8210434477fac721bd08de885c8371a6da3807c
Abdurrahman Ansari <abdurrahmanansari03@gmail.com>
2024-07-17 16:07:33 +0530

    Create 4_Variables.py

---
+++ 4_Variables.py
@@ -0,0 +1,24 @@
+###################### Variables in Python ######################
+
+# Variables in python are used to store data. it works as a temporary memory to store the data.
+
+# Lets learn about the variables using the example.
+
+'''
+Lets suppose you have many types of foods. some are big, some are small, some are solid, some are liquid, some are hot, some are cool, .
+You can not put the big food in the small container(box).
+You can't put the small food in the bigger box as it will waste remaining space in the box.
+You can't put the liquid food in the box with holes or in paper box.
+You can't put the hot food in the plastic box or any box which has melting property.
+You can't put the cool food in the paper box or any box which can be damaged with the cold temperature.
+'''
+# Same Python has the types of variables to store different types of data (food in example).
+
+###################### Examples ######################
+Variable = "variable can store any types of Data. But one type of data at once"
+
+Variable_Naming = "Variable can only be named in the python specified format.\nAccepted naming for variables in python.\nNames starting from english alphabets: English\nNames starting with underscore: _Variable, _1234, _#$@^& etc."
+
+
+
+String_Variable = "Anything in between the single/double quotes are taken as string (text/Series of characters).\nWhether it is a number like 1,-6,0, 0.44543.\nor symbols like ! @ # $ % ^ & * ( ) _ +\nor just empty space like       ."
